a03f48231c0d8e44a651da8ed19dcfa1804cab23
mfraggy25 <mfraggy25@users.noreply.github.com>
2019-10-30 20:53:29 -0400

    Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 var repository = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] }
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Ivysaur", height: 0.9, types: ["grass", "poison"] },
+  { name: "Venusaur", height: 1.8, types: ["grass", "poison"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
+  { name: "Charmeleon", height: 0.9, types: ["fire"] },
+  { name: "Charizard", height: 1.5, types: ["fire", "flying"] },
+  { name: "Squirtle", height: 0.3, types: ["water"] },
+  { name: "Wartortle", height: 0.9, types: ["water"] }
 ];
