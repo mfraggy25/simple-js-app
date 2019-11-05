f380528ea7c553c8855b4e1e33d77b2bf71e9f5c
mfraggy25 <mfraggy25@users.noreply.github.com>
2019-11-04 20:28:21 -0500

    Wrap repository around IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-var repository = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Ivysaur", height: 0.9, types: ["grass", "poison"] },
-  { name: "Venusaur", height: 1.8, types: ["grass", "poison"] },
-  { name: "Charmander", height: 0.6, types: ["fire"] },
-  { name: "Charmeleon", height: 0.9, types: ["fire"] },
-  { name: "Charizard", height: 1.5, types: ["fire", "flying"] },
-  { name: "Squirtle", height: 0.3, types: ["water"] },
-  { name: "Wartortle", height: 0.9, types: ["water"] }
-];
+var pokemonRepository = (function() {
+  var repository = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Ivysaur", height: 0.9, types: ["grass", "poison"] },
+    { name: "Venusaur", height: 1.8, types: ["grass", "poison"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+    { name: "Charmeleon", height: 0.9, types: ["fire"] },
+    { name: "Charizard", height: 1.5, types: ["fire", "flying"] },
+    { name: "Squirtle", height: 0.3, types: ["water"] },
+    { name: "Wartortle", height: 0.9, types: ["water"] }
+  ];
+
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 repository.forEach(function(i) {
   var size;
